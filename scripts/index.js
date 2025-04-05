148c28506b5c940f68539fb34085066c95eecfc2
Guilherme Olimpio Rodrigues <guizinholimpio1408@outlook.com>
2025-04-05 10:57:13 -0300

    Implementação do carrinho lateral de compras, ainda não está 100%

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const cartButton = document.getElementById("cart-button");
+const cartSidebar = document.getElementById("cart-sidebar");
+const closeCartButton = document.getElementById("close-cart");
+
+// para abrir o carrinho
+cartButton.addEventListener("click", () => {
+  cartSidebar.classList.add("cart-visible");
+  cartSidebar.classList.remove("cart-hidden");
+});
+
+// para fechar o carrinho
+closeCartButton.addEventListener("click", () => {
+  cartSidebar.classList.remove("cart-visible");
+  cartSidebar.classList.add("cart-hidden");
+});
+
+// para fechar o carrinho ao clicar fora dele
